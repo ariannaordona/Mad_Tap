096be57a0e64cda778f56581443ff65dc370cea3
ariannaordona <ariannaordona@gmail.com>
2020-11-04 20:39:42 -0700

    Separated onCreate code into functions setListeners and setButtonText

--- app/src/main/java/com/example/madtap/MainActivity.kt
+++ app/src/main/java/com/example/madtap/MainActivity.kt
@@ -30,15 +30,12 @@ class MainActivity : AppCompatActivity() {
 
     private val vocabWord = arrayOf(apple, banana, lemon, orange)
 
-    val r = Random()
+    private val r = Random()
     var answerObject = vocabWord[r.nextInt(vocabWord.size)]
     var answer = answerObject.name
     var answerArray = ArrayList<Word>()
 
-    //TODO make the imageviews visually slide down like rolling
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(TAG, "OnCreate: called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -57,68 +54,11 @@ class MainActivity : AppCompatActivity() {
         tv_answerNumber.text = answer.length.toString()
         tv_answerHint.text = makeVertical(answerObject.name)
 
-
-        //Setting all input buttons to listeners
-        val listener = View.OnClickListener { v ->
-            val b = v as Button
-            tv_answer.append(b.text)
- //           et_answer.append("\n")
-        }
-
-        b_1.setOnClickListener(listener)
-        b_2.setOnClickListener(listener)
-        b_3.setOnClickListener(listener)
-        b_4.setOnClickListener(listener)
-        b_5.setOnClickListener(listener)
-        b_6.setOnClickListener(listener)
-        b_7.setOnClickListener(listener)
-        b_8.setOnClickListener(listener)
-
-        b_clear.setOnClickListener {
-            tv_answer.text = ""
-        }
-
-        //populates buttons with vocab chars based on a shuffled array of 8
-        val buttons = arrayOf(0, 1, 2, 3, 4, 5, 6 , 7)
-        buttons.shuffle()
-        //v is all the vocab chars in one string and is used to populates buttons with 1 char
-        val sb = StringBuilder()
-        for (i in vocab) {
-            sb.append(i)
-        }
-        val v = sb.toString()
-        b_1.text = v[buttons[0]].toString()
-        b_2.text = v[buttons[1]].toString()
-        b_3.text = v[buttons[2]].toString()
-        b_4.text = v[buttons[3]].toString()
-        b_5.text = v[buttons[4]].toString()
-        b_6.text = v[buttons[5]].toString()
-        b_7.text = v[buttons[6]].toString()
-        b_8.text = v[buttons[7]].toString()
-
-        //checks et_answer whenever the text is changed
-        val textWatcher = object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-            override fun afterTextChanged(p0: Editable?) {
-               val v = p0.toString()
-                // if the length of user input is the same length as the answer, will check if the user input is correct
-                // when it is correct, will call function correctAnswer to update game, if incorrect will clear et_answer
-                if (v.length == answer.length) {
-                    if (v == answer) {correctAnswer()}
-                    else {
-                        tv_answer.text = ""
-                        incorrectAnswer()
-                    }
-                }
-            }
-        }
-        tv_answer.addTextChangedListener(textWatcher)
+        setListeners()
+        setButtonText()
     }
 
     private fun correctAnswer() {
-        Log.d(TAG, "correctAnswer: called")
-
         imageAnimation()
         tv_answer.text = ""
         answerArray.removeAt(0)
@@ -140,7 +80,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun incorrectAnswer() {
-        Log.d(TAG, "incorrectAnswer: called")
         tv_answer.text = ""
         //TODO add a red highlight flash or a red X on the image to give user feedback that their answer was not correct
     }
@@ -173,7 +112,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun makeVertical(v: String): String {
-        Log.d(TAG, "makeVertical: called")
         val sb = StringBuilder()
         for(i in v.indices) {
             sb.append(v[i])
@@ -183,8 +121,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun imageAnimation() {
-        Log.d(TAG, "imageAnimation: called")
-
         val animation = AnimationUtils.loadAnimation(this, R.anim.slide)
         iv_1.startAnimation(animation)
         iv_2.startAnimation(animation)
@@ -192,4 +128,60 @@ class MainActivity : AppCompatActivity() {
         iv_4.startAnimation(animation)
     }
 
+    private fun setListeners() {
+        val listener = View.OnClickListener { v ->
+            val b = v as Button
+            tv_answer.append(b.text)
+        }
+
+        b_1.setOnClickListener(listener)
+        b_2.setOnClickListener(listener)
+        b_3.setOnClickListener(listener)
+        b_4.setOnClickListener(listener)
+        b_5.setOnClickListener(listener)
+        b_6.setOnClickListener(listener)
+        b_7.setOnClickListener(listener)
+        b_8.setOnClickListener(listener)
+
+        b_clear.setOnClickListener {
+            tv_answer.text = ""
+        }
+        //textWatcher checks et_answer whenever the text is changed
+        // if the length of user input is the same length as the answer it will check if the user input is correct
+        // when answer is correct will call function correctAnswer and incorrectAnswer if incorrect
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {
+                val v = p0.toString()
+                if (v.length == answer.length) {
+                    if (v == answer) {correctAnswer()}
+                    else {
+                        tv_answer.text = ""
+                        incorrectAnswer()
+                    }
+                }
+            }
+        }
+        tv_answer.addTextChangedListener(textWatcher)
+    }
+
+    private fun setButtonText() {
+        //populates buttons with vocab chars based on a shuffled array of 8
+        val buttons = arrayOf(0, 1, 2, 3, 4, 5, 6 , 7)
+        buttons.shuffle()
+        //StringBuilder is used to combine all vocab characters into one variable v
+        //Then v is used to populate all the buttons with a single different character
+        val sb = StringBuilder()
+        for (i in vocab) { sb.append(i) }
+        val v = sb.toString()
+        b_1.text = v[buttons[0]].toString()
+        b_2.text = v[buttons[1]].toString()
+        b_3.text = v[buttons[2]].toString()
+        b_4.text = v[buttons[3]].toString()
+        b_5.text = v[buttons[4]].toString()
+        b_6.text = v[buttons[5]].toString()
+        b_7.text = v[buttons[6]].toString()
+        b_8.text = v[buttons[7]].toString()
+    }
 }
